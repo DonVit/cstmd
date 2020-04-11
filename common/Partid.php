7b6ae9ac6ff484c63b96bdf795ccc62b7bd60cb6
Vitalie Doni <vitalie.doni@gmail.com>
2020-04-11 21:33:29 +0300

    add primar alegeri locale

--- common/Partid.php
+++ common/Partid.php
@@ -1,5 +1,5 @@
 <?php
-class Location extends DBManager {
+class Partid extends DBManager {
 	public $id;
 	public $alegeri_id;
 	public $partidcod;
@@ -10,9 +10,9 @@ class Location extends DBManager {
 		return "al_partid";
 	}
 			
-	public static function getPartidByAlegeriAndParidCod($alegeriid,$partidcod){
+	public static function getPartidByParidCod($partidcod){
 		$p=new Partid();
-		$ps=$p->getAll("alegeri_id=".$alegeriid." and partidcod=".$partidcod);
+		$ps=$p->getAll("alegeri_id=".Primar::getLastAlegeriYear()->alegeri_id." and partidcod=".$partidcod);
 		
 		if (!is_null($ps)){
 			return $ps[0];
@@ -20,6 +20,10 @@ class Location extends DBManager {
 			return null;
 		}		
 	}
-
+	function getPrimarPartidByLocationId($localitate_id){
+		$sql="SELECT a2.* FROM al_primari as a1 inner join al_partid as a2 on a1.alegeri_id=a2.alegeri_id and a1.partid=a2.partidcod where a1.alegeri_id=".Primar::getLastAlegeriYear()->alegeri_id." and a1.localitate_id=".$localitate_id;
+		$ls=$this->doSql($sql);
+		return $ls[0];
+	}	
 }
 ?>
